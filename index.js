5d949f1308d83cf67f374bb7a7fb55375e5ee752
rroopesh <hrroopesh639@gmail.com>
2021-05-24 10:32:19 +0530

    commit

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const bodyParser = require('body-parser');
 const couchbase = require("couchbase");
 const User = require('./models/Users');
 const Uuid = require("uuid");
-const { response } = require('express');
+const  response  = require('express');
 
-var cluster = new couchbase.Cluster("couchbase://127.0.0.1",{
+var cluster = new couchbase.Cluster("couchbase://localhost",{
     username: 'Administrator',
-    password: 'password',
+    password: 'Password'
 });
 
 
@@ -17,7 +17,7 @@ var bucket = cluster.bucket("register");
 const collection = bucket.defaultCollection();
 const app = express();
 
-
+app.use(bodyParser.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}))
